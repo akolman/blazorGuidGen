c505d460c0d58f6ce76559375edc6e8d770c43d2
Andrew Kolman <Andrew.Kolman@hyland.com>
2023-10-03 11:54:46 -0500

    Removed HttpClient injection

--- BlazorGuidGenerator/Program.cs
+++ BlazorGuidGenerator/Program.cs
@@ -29,7 +29,6 @@ namespace BlazorGuidGenerator
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<ClipboardService>();
 
             await builder.Build().RunAsync();
